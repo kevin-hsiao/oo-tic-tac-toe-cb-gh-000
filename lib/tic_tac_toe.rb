b9f24c630441d11b88442096c4e4756e2d3f9037
Kevin Hsiao <kevin.hsiao@mail.utoronto.ca>
2019-09-06 23:54:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,10 +22,10 @@ class TicTacToe
   end
   
   def position_taken?(index)
-    board[index] == "X" || board[index] == "O"
+    @board[index] == "X" || @board[index] == "O"
   end
   
   def valid_move?(index)
-    !position_taken?(board, index) && index.between?(0, 8)
+    !position_taken?(@board, index) && index.between?(0, 8)
   end
 end
