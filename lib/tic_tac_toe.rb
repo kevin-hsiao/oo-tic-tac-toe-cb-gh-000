8eda20f3fa23f6398df34d8420137a99a4721c4a
Kevin Hsiao <kevin.hsiao@mail.utoronto.ca>
2019-09-07 00:02:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,5 +54,6 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.find {|combo| @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0])}
+  end
 
 end
